71d49af7ed5fa9298324059dae2f2a6b83b65c3c
= <=>
2021-12-24 15:56:14 -0500

    made class and added iterator functionalilty

--- cardgenartor.py
+++ cardgenartor.py
@@ -7,21 +7,20 @@ import time
 CARD_NUM_LENGTH = 15
 NUM_OF_NAMES = 31127
 
-class cardGenarator():
-    def __init__(self, CARD_NUM_LENGTH = 15, NUM_OF_NAMES = 31127):
+class CardGenarator():
+    def __init__(self, DEFAULT_CARD_TYPE = False, CARD_NUM_LENGTH = 15, NUM_OF_NAMES = 31127):
+        """"""
         self.CARD_NUM_LENGTH = CARD_NUM_LENGTH
         self.NUM_OF_NAMES = NUM_OF_NAMES
 
-        self.card_map = {
-                'visa': [4],
-                'mastercard': [51, 52, 53, 54],
-                'american express': [34, 37],
-                'amex': [34, 37],
-                'jbc': [34, 37],
-                'discovery': [34, 37],
-                'discover': [34, 37]
-                }
-
+        self.card_map = { 'visa': [4],
+                        'mastercard': [51, 52, 53, 54],
+                        'american express': [34, 37],
+                        'amex': [34, 37],
+                        'jbc': [34, 37],
+                        'discovery': [34, 37],
+                        'discover': [34, 37]
+                        }
 
     def rand_num_w_zero(self, num_of_digits):
         digit_str = ""
@@ -30,32 +29,44 @@ class cardGenarator():
             digit_str += str(random.randint(0,9))
 
         return digit_str
-    
-    def get_card_in(self):
-        card_issuer = input("Choose a card type: Visa, Mastercard, American Express, JBC, or Discovery")
-        card_issuer = card_issuer.lower()
-        card_num_prefix_list = card_map[card_issuer]
 
-    def gen_card(self):
-        card_num_prefix_list = random.choice(list(self.card_map.items()))
-        card_num_prefix = random.choice(card_num_prefix_list[1])
+    def gen_many_cards(self, n):
+        """Generate a given number of cards and output\nCalles __iter__"""
+        for i in n:
+            return self.__next__()
+
+    def __iter__(self):
+        """Retuns self for iteration"""
+        return self
+
+    def __next__(self):
+        """calls gen_one_card"""
+        return self.gen_one_card()
+
+    def gen_one_card(self):
+        """Generate a random card"""
+        card_num_prefix_item = random.choice(list(self.card_map.items()))
+
+        card_type = card_num_prefix_item[0]
+        card_num_prefix = random.choice(card_num_prefix_item[1])
 
         len_to_gen = self.CARD_NUM_LENGTH - len(str(card_num_prefix))
-        card_num = self.rand_num_w_zero(len_to_gen)
+        rand_card_num = self.rand_num_w_zero(len_to_gen)
+        card_num = str(card_num_prefix) + str(rand_card_num) 
 
         with open('./posnames.csv') as names_file:
-            names_csv = csv.reader(names_file) 
-            names_list = list(names_csv) 
+            names_source_csv = csv.reader(names_file) 
+            names_source_list = list(names_source_csv) 
             names = []
 
             for i in range(3):
                 name_index = random.randint(0, self.NUM_OF_NAMES)
-                names.append(names_list[name_index][0])
+                names.append(names_source_list[name_index][0])
             
             name_str = str(names[0]) + ' ' + str(names[1][0]) + ' ' + str(names[2])
 
-        cvv = rand_num_w_zero(3)
-        pin = rand_num_w_zero(4)
+        cvv = self.rand_num_w_zero(3)
+        pin = self.rand_num_w_zero(4)
 
         current_year = time.gmtime(time.time()).tm_year
 
@@ -63,17 +74,20 @@ class cardGenarator():
         ydate = str(random.randint(current_year,current_year + 5))
         date = mdate + '/' + ydate
         
-        if format_json:
-            return_item = {
-                    'card_number': card_num,
-                    'name': name_str,
-                    'cvv': cvv,
-                    'pin': pin,
-                    'date': date
-                    }
-        else:
-            return_item  
+        return_json = {
+                'card_number': card_num,
+                'name': name_str,
+                'cvv': cvv,
+                'pin': pin,
+                'date': date
+                }
 
-        return(card_num, name_str, cvv, pin, date)
+        return return_json
 
+if __name__ == "__main__":
+    card_gen_instance = CardGenarator()
+    card_gen_instance.gen_one_card()
 
+    for x, i in enumerate(card_gen_instance):
+        print(i)
+        if x == 100: break
